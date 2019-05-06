09d437b7d26e22c3279095354b3bc2c996c4cd48
Jacob Williams <jacobaw@gmail.com>
2019-05-05 19:19:39 -0500

    import problem 1 solution 1

---
+++ src/main/kotlin/problem0001/solution1/p1s1.kt
@@ -0,0 +1,42 @@
+package problem0001.solution1
+
+/*
+Problem statement from https://projecteuler.net/problem=1
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
+// In this approach, I keep track of the current multiple of 3 and the current
+// multiple of 5, and the current sum. Then I loop until all multiples have
+// been added into the sum. Each iteration of the loop increments *either*
+// the threes variable, *or* the fives variable, before adding it to the sum.
+// To avoid adding the same number twice (when it's a multiple of both 3 and 5)
+// the loop ensures that the threes variable always reaches any given number
+// first, and when the threes and fives variables are equal the number is not
+// added to the sum (since it will already have been added when the threes
+// variable was updated).
+
+fun main() {
+    var threes = 3
+    var fives = 5
+    var sum = threes + fives
+    val end = 1000
+
+    while (threes < end || fives < end) {
+        if (threes < fives) {
+            threes += 3
+            if (threes < end && threes != fives) {
+                sum += threes
+            }
+        } else {
+            fives += 5
+            if (fives < end && threes != fives) {
+                sum += fives
+            }
+        }
+    }
+
+    println(sum)
+}
